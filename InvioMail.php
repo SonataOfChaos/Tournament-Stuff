0b8c275af6f906bfb0488b9117cbbabc635c8a69
Jhyrachy <jacopopieri@gmail.com>
2015-05-13 17:16:21 +0200

    Bugfix

--- InvioMail.php
+++ InvioMail.php
@@ -11,25 +11,30 @@
 <br>
 
 <?php
-    $jsonData = '{"marco":"marco@test.it", "giovanni":"giovanni@mail.it"}';
-    $json = json_decode($jsonData, true);
 	
-	$opts = '';
-	foreach($json as $name => $email)
-	{
-	$opts .= '<option value="'.$email.'">'.$name.'</option>';
+	$filename = "teamContact.json";
+	if(file_exists($filename) && $string=file_get_contents($filename) !== false){
+		$jsonData = file_get_contents($filename);
+		$json = json_decode($jsonData, true);
+	
+		$opts = '';
+		foreach($json as $name => $email)
+		{
+			$opts .= '<option value="'.$email.'">'.$name.'</option>';
+		}
+		
+		echo ' <select name="Team1">'.$opts.'</select> <br> ';
+		echo ' <select name="Team2">'.$opts.'</select> <br>';
 	}
-	
-	echo ' <select name="Team1">'.$opts.'</select> <br> ';
-	echo ' <select name="Team2">'.$opts.'</select> <br>';
-	
+	else
+		echo 'Nessuna lista contatti disponibile! <br>';
 ?>
 <br>
 Soggetto: <input type="text" name="soggetto" required><br>
 <br>
 Messaggio: <input type="text" name="messaggio" style="width: 300px" required><br>
 <br>
-<center><input type="submit" value=" Invia "></center><br>
+<center><input type="submit" value=" Invia " ></center><br>
 </form>
 
 </body>
